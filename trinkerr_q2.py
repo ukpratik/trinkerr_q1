f10017063e9ddf97c919bff408737a259a5a9421
Pratik Ukey <34273491+ukpratik@users.noreply.github.com>
2022-04-09 11:01:33 +0530

    Create trinkerr_q2.py

---
+++ trinkerr_q2.py
@@ -0,0 +1,25 @@
+
+def getGroups(keys):
+    res = []
+    done_index = []
+    for i in range(len(keys)):
+        if keys[i] in done_index:
+            continue
+        temp = []
+        temp.append(keys[i])
+        temp_key_1 = keys[i]
+        for j in range(i+1,len(keys)):
+            if j not in done_index:
+                temp_key_2 = keys[j]
+                if temp_key_1.sort() == temp_key_2.sort():
+                    temp.append(keys[j])
+                    done_index.append(j)
+        res.append(temp)
+
+    print(res)
+
+
+if __name__ == "__main__":
+    # here the below keys are just example, this can be taken in input
+    keys = ["idea", "idae", "bsnl", "nsbl", "grasim", "bata"]
+    getGroups(keys)
